c7abd85c506da422f5622c935228d5419ab4d031
Kolya <NickTitov123@yandex.ru>
2023-08-14 16:36:20 +0300

    Refactor main.py

--- main.py
+++ main.py
@@ -11,22 +11,25 @@ from database import Database
 from middlewares import DatabaseMiddleware
 
 
+async def on_startup(dispatcher):
+    print(f'[{dt.datetime.now().strftime("%d/%b/%y %H:%M:%S")}] Бот запущен за {datetime.now() - STARTUP}')
+    db = Database(HOST, PORT, DATABASE, USER, PASSWORD)
+    await db.create_pool()
+    dispatcher.update.middleware(DatabaseMiddleware(db))
+
+
+async def on_shutdown(dispatcher):
+    print(f'[{dt.datetime.now().strftime("%d/%b/%y %H:%M:%S")}] Бот отключён, время работы: {datetime.now() - STARTUP}')
+
+
 async def main():
     bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
     storage = MemoryStorage()
     dp = Dispatcher(storage=storage)
     dp.include_routers(cancel_state.router, inliner.router, commands.router, post.router)
 
-    @dp.startup()
-    async def on_startup():
-        print(f'[{dt.datetime.now().strftime("%d/%b/%y %H:%M:%S")}] Бот запущен за {datetime.now() - STARTUP}')
-        db = Database(HOST, PORT, DATABASE, USER, PASSWORD)
-        await db.create_pool()
-        dp.update.middleware(DatabaseMiddleware(db))
-
-    @dp.shutdown()
-    async def on_shutdown():
-        print(f'[{dt.datetime.now().strftime("%d/%b/%y %H:%M:%S")}] Бот отключён, время работы: {datetime.now() - STARTUP}')
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
